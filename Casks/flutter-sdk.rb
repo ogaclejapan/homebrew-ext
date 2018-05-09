bcd00255ad49ba2312612d3d2affb99c2764cc32
ogaclejapan <ogaclejapan@gmail.com>
2018-05-09 18:05:18 +0900

    Update flutter-sdk cask
    Improve '.pub-cache' directory path so that it does not depend on a specific version

--- Casks/flutter-sdk.rb
+++ Casks/flutter-sdk.rb
@@ -7,5 +7,40 @@ cask 'flutter-sdk' do
   name 'Flutter SDK'
   homepage 'https://flutter.io/'
 
-  binary 'flutter/bin/flutter'
+  # shim script (https://github.com/caskroom/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/flutter.wrapper.sh"
+  binary shimscript, target: 'flutter'
+
+  preflight do
+    # Use warm cache: $sdk/.pub-cache
+    # https://github.com/flutter/flutter/blob/master/bin/flutter#L97-L99
+    # https://github.com/flutter/flutter/blob/master/dev/bots/prepare_package.dart#L212
+    # https://github.com/flutter/flutter/blob/master/dev/bots/prepare_package.dart#L322
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      TOOL="#{staged_path}/flutter/bin/flutter"
+      if [[ -d "#{HOMEBREW_PREFIX}/share/flutter-sdk/.pub-cache" ]]; then
+        PUB_CACHE="#{HOMEBREW_PREFIX}/share/flutter-sdk/.pub-cache" exec "$TOOL" "$@"
+      else
+        exec "$TOOL" "$@"
+      fi
+    EOS
+  end
+  
+  postflight do
+    FileUtils.ln_sf("#{staged_path}/flutter", "#{HOMEBREW_PREFIX}/share/flutter-sdk")
+  end
+
+  uninstall_postflight do
+    FileUtils.rm("#{HOMEBREW_PREFIX}/share/flutter-sdk")
+  end
+
+  caveats do
+    <<~EOS
+      You may want to add to your profile:
+
+        'export FLUTTER_SDK_ROOT="#{HOMEBREW_PREFIX}/share/flutter-sdk"'
+    EOS
+  end
+  
 end
